daa144f8fafa86d4de9cf3952e070b51d3b845c8
SvetlanaRiasnova <R_S_U@outlook.com>
2024-06-01 14:07:51 +1000

    code added

---
+++ interno/src/components/BaseBanner.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="banner">
+    <div class="banner__info">
+      <h1 class="banner__info_header">{{ bunnerHeader }}</h1>
+      <div class="banner__info_breadcrump">Home / Blog</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BaseBanner",
+  props: {
+    bunnerHeader: String,
+    imagePath: String,
+  },
+  data() {},
+};
+</script>
+
+<style lang="scss">
+$familyTitle: "DM Serif Display", serif;
+$familyText: "Jost", sans-serif;
+$headerColor: #292f36;
+$textColor: #4d5053;
+
+@mixin flexCenter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@mixin logoImage {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+@mixin logoText {
+  font-family: $familyTitle;
+  font-size: 40px;
+  font-weight: 400;
+  color: $headerColor;
+}
+
+@mixin h2Style {
+  font-family: $familyTitle;
+  color: $headerColor;
+  font-size: 50px;
+  font-weight: 400;
+  line-height: 62.5px;
+}
+
+* {
+  box-sizing: border-box;
+  font-family: $familyText;
+  color: $textColor;
+}
+.banner {
+  // background-image: url('${imagePath}');
+  min-height: 356px;
+  @include flexCenter;
+  align-items: flex-end;
+  &__info {
+    text-align: center;
+    background-color: #fff;
+    padding: 41px;
+    border-radius: 37px 37px 0px 0px;
+    &_header {
+      @include h2Style;
+      text-align: left;
+    }
+    &_breadcrump {
+      font-size: 22px;
+      font-weight: 400;
+      line-height: 33px;
+    }
+  }
+}
+</style>
---
+++ interno/src/components/pages/BlogDetailsPage.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <div class="banner__blog_details"></div>
+    <section class="articles__list">
+      <BlogDetailsComponent></BlogDetailsComponent>
+    </section>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "../HeaderComponent.vue";
+import BlogDetailsComponent from "../BlogDetailsComponent.vue";
+import FooterComponent from "../FooterComponent.vue";
+
+export default {
+  name: "BlogDetailsPage",
+  components: {
+    HeaderComponent,
+    BlogDetailsComponent,
+    FooterComponent,
+  },
+
+  data() {},
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss">
+$familyTitle: "DM Serif Display", serif;
+$familyText: "Jost", sans-serif;
+$headerColor: #292f36;
+$textColor: #4d5053;
+
+@mixin flexCenter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@mixin logoImage {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+@mixin logoText {
+  font-family: $familyTitle;
+  font-size: 40px;
+  font-weight: 400;
+  color: $headerColor;
+}
+
+@mixin h2Style {
+  font-family: $familyTitle;
+  color: $headerColor;
+  font-size: 50px;
+  font-weight: 400;
+  line-height: 62.5px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: $familyText;
+  color: $textColor;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.banner__blog_details {
+  background-image: url("../../assets/img/Banner_DetailsBlog.png");
+  min-height: 356px;
+}
+
+.blog__page {
+  display: flex;
+  padding-top: 197px;
+  gap: 52px;
+  &_wrapper {
+    width: 800px;
+  }
+  &_article {
+    padding-bottom: 21px;
+    &-title {
+      padding-bottom: 21px;
+      color: $headerColor;
+      font-family: $familyTitle;
+      font-size: 50px;
+      font-weight: 400;
+      line-height: 125%;
+      text-align: left;
+    }
+    &-img {
+      border-radius: 50px;
+    }
+    &-text {
+      padding-bottom: 38px;
+      font-size: 22px;
+      font-weight: 400;
+      line-height: 150%;
+      text-align: left;
+    }
+    &-info {
+      padding-top: 46px;
+      padding-bottom: 48px;
+      display: flex;
+      justify-content: space-between;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 150%;
+      text-align: left;
+      text-transform: capitalize;
+    }
+    &-link {
+      padding-right: 60px;
+    }
+    &-quote {
+      @include flexCenter;
+      width: 799px;
+      height: 267px;
+      flex-direction: column;
+      padding: 53px 227px 53px 227px;
+      border-radius: 50px;
+      background: #f4f0ec;
+    }
+    &-simbol {
+      color: #cda274;
+      font-family: $familyTitle;
+      font-style: italic;
+      font-size: 200px;
+      font-weight: 400;
+      line-height: 125%;
+      text-align: center;
+      padding: 0;
+    }
+    &-quote-text {
+      width: 345px;
+      color: #cda274;
+      font-family: $familyTitle;
+      font-style: italic;
+      font-size: 25px;
+      font-weight: 400;
+      line-height: 125%;
+      text-align: center;
+      margin-top: -100px;
+    }
+    &-ordered-description {
+      padding-left: 30px;
+    }
+  }
+}
+.blog__tags {
+  width: 345px;
+  &_header {
+    color: $headerColor;
+    font-family: $familyTitle;
+    font-size: 25px;
+    font-weight: 400;
+    line-height: 125%;
+    text-align: left;
+    padding-bottom: 24px;
+  }
+  &_btns {
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 11px;
+  }
+  &_btn {
+    padding: 9px 30px 9px 30px;
+    border: none;
+    border-radius: 10px;
+    background: rgb(244, 240, 236);
+    color: #000000;
+    margin: 0px 10px;
+    &:focus {
+      background: #292f36;
+      color: #ffffff;
+    }
+  }
+}
+ol li::marker {
+  color: #cda274;
+  font-family: $familyTitle;
+  padding-left: 14px;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 125%;
+  text-align: left;
+}
+</style>
